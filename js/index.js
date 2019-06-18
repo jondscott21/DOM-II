bb3b85cd9a9cdcafc9b5638e68cd02806686691f
Jonathan Scott <jonathan.scott@gmail.com>
2019-06-18 12:37:31 -0700

    Added nav and logo events

--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
 // Your code goes here
-console.log('test');
+const navAnchors = document.querySelector('nav');
+console.log(navAnchors);
+
+// #### Double click scroll to top on fun bus ####
+const funBus = document.querySelector('.logo-heading');
+funBus.addEventListener('dblclick', event => {
+    window.scroll(0, 0);
+})
+
+//#### Nav Anchor Color change ####
+navAnchors.addEventListener('mouseover', event => {
+    event.target.style.color = 'blue';
+})
+
+navAnchors.addEventListener('mouseout', event => {
+    event.target.style.color = 'black';
+})
+
+

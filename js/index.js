0fc0c9cb66e6ba81d5ea70694c1b70da829b3a4b
Jonathan Scott <jonathan.scott@gmail.com>
2019-06-18 18:11:49 -0700

    finished mvp

--- js/index.js
+++ js/index.js
@@ -3,10 +3,14 @@
 
 // #### Window ####
 
+
+//Scroll
 const home = document.querySelector('.home');
 window.addEventListener('scroll', event => {
     home.style.background = 'antiquewhite';
 })
+
+// Resize
 window.addEventListener('resize', event =>{
     if(window.innerWidth <= 550) {
         home.style.background = 'lightblue';
@@ -15,6 +19,8 @@ window.addEventListener('resize', event =>{
         home.style.background = '';
     }
 })
+
+// Keydown
 window.addEventListener('keydown', event => {
     event.target.style.color = 'pink';
 })
@@ -22,6 +28,11 @@ window.addEventListener('keydown', event => {
 window.addEventListener('keyup', event => {
     event.target.style.color = 'black';
 })
+
+// Load
+window.addEventListener('load', (event) => {
+    // alert('Page Loaded');
+});
 
 
 
@@ -48,8 +59,9 @@ nav.addEventListener('click', event => {
     event.preventDefault();
 })
 
-const welcome = document.querySelector('.intro p');
 
+// Wheel
+const welcome = document.querySelector('.intro p');
 welcome.addEventListener('wheel', event => {
     event.preventDefault();
     let scale = 1;
@@ -57,54 +69,62 @@ welcome.addEventListener('wheel', event => {
     scale = Math.min(Math.max(.125, scale), 4);
     event.target.style.transform = `scale(${scale})`;
 })
-// console.log(welcome.textContent);
-// welcome.addEventListener('select', event => {
-//     event.target.style.background ='red';
-//     console.log("we made it");
-//     console.log(event.target.textContent.substring(event.target.selectionStart, event.target.selectionEnd))
-//     event.target.textContent.substring(event.target.selectionStart, event.target.selectionEnd).style.color = 'red'
-// })
-
-
-function doSwap(a, b) {
-    swapElements(document.getElementById(a), document.getElementById(b));
-}
-
-function swapElements(obj1, obj2) {
-    // save the location of obj2
-    let parent2 = obj2.parentNode;
-    let next2 = obj2.nextSibling;
-    // special case for obj1 is the next sibling of obj2
-    if (next2 === obj1) {
-        // just put obj1 before obj2
-        parent2.insertBefore(obj1, obj2);
-    } else {
-        // insert obj2 right before obj1
-        obj1.parentNode.insertBefore(obj2, obj1);
-
-        // now insert obj1 where obj2 was
-        if (next2) {
-            // if there was an element after obj2, then insert obj1 right before that
-            parent2.insertBefore(obj1, next2);
-        } else {
-            // otherwise, just append as last child
-            parent2.appendChild(obj1);
-        }
-    }  
-}
-const destination = document.querySelectorAll('.destination');
-destination.forEach(el => {
-    el.addEventListener('drag', event => {
-        let dragged = event.target;
-
-        swapElements(el,)
-    })
+welcome.addEventListener('click', event => {
+    let scale = 1;
+    event.target.style.transform = `scale(${scale})`;
+})
+console.log(welcome.textContent);
+welcome.addEventListener('select', event => {
+    event.target.style.background ='red';
 })
 
-const box = document.getElementsByClassName('box')[0]
-const containers = document.getElementsByClassName('holder')
-for(const container of containers) {
-  container.addEventListener("dragover", dragover)
-  container.addEventListener("dragenter", dragenter)
-  container.addEventListener("drop", drop)
-}
+// Focus
+const firstAnchor = document.querySelector('.nav a')
+console.log(firstAnchor);
+firstAnchor.addEventListener('focus', event => {
+    event.target.style.fontSize = '5rem';
+})
+firstAnchor.addEventListener('blur', event => {
+    event.target.style.fontSize = '';
+})
+
+// Drag/Drop
+const busImg = document.querySelector('.intro img');
+busImg.addEventListener('dragstart', event => {
+    welcome.style.color = 'green';
+    console.log('dragged');  
+    
+});
+busImg.addEventListener('dragend', event => {
+    console.log('dropped');
+    welcome.style.color = 'red';   
+});
+
+// Select
+const inputBar = document.createElement('input')
+inputBar.value = `Select me!`;
+home.appendChild(inputBar)
+console.log(inputBar);
+inputBar.addEventListener('select', event => {
+    alert('selected this');
+})
+
+// Propagation chain
+
+const contentDestination = document.querySelector('.content-pick');
+const destination = document.querySelector('.destination');
+const btn = document.querySelector('.btn');
+console.log(contentDestination);
+
+contentDestination.addEventListener('click', event => {
+    event.stopPropagation();
+    alert(`clicked top div`);
+})
+destination.addEventListener('click', event => {
+    event.stopPropagation();
+    alert(`clicked middle div`);
+})
+btn.addEventListener('click', event => {
+    event.stopPropagation();
+    alert(`clicked bottom div`);
+});

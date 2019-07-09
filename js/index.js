0ff7cb2e4f8a2853d2d932926f9596bb266e030d
Jonathan Scott <jonathan.scott@gmail.com>
2019-07-09 14:49:48 -0700

    added drag/drop, load, focus, resize, and scroll

--- js/index.js
+++ js/index.js
@@ -23,6 +23,45 @@ document.addEventListener('wheel', event => {
     }  
 })
 
-// Drag & Drop
-const busImg = document.querySelector('header > img')
-console.log(busImg)
+// Drag & Drop bus image to change text below it
+const busImg = document.querySelector('.intro > img')
+busImg.addEventListener('dragstart', event => {
+    const welcome = document.querySelector('.intro > h2')
+    welcome.textContent = `OH GOD PLEASE HELP!`
+    event.target.addEventListener('dragend', () => welcome.textContent = `Thanks for dropping me`)
+});
+
+// Load text in header 
+window.addEventListener('load', () => funBus.textContent = `${funBus.textContent}... Has Loaded`)
+
+// Focus
+navAnchors = Array.from(document.querySelectorAll('.nav > a'));
+navAnchors.forEach(el => el.addEventListener('focus', event => {
+    let savedTag = event.target.textContent;
+    event.target.textContent = `CLICKED`;
+    event.target.addEventListener('blur', () => event.target.textContent = savedTag);
+
+}))
+
+// Resize window to mobile background change
+
+window.addEventListener('resize', event =>{
+    if (window.innerWidth <= 500) {
+        document.body.style.background = 'antiquewhite';
+    } else if (window.innerWidth <= 800) {
+        document.body.style.background = 'powderblue';
+    } else {
+        document.body.style.background = '';
+    }
+        
+});
+
+// scroll
+window.addEventListener('scroll', event =>{
+    funBus.textContent = `Fun Bus is scrolling`
+    setInterval(function() {
+        funBus.textContent = `Fun Bus stopped scrolling`
+    }, 300)
+});
+
+// Select
